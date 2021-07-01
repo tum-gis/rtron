d86dba5789a3e2432bc1bb3aab98fab4bdc44042
Benedikt <benedikt.schwab@tum.de>
2021-07-01 08:48:01 +0200

    updated to current kotlin version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -18,8 +18,8 @@
 object DependencyVersions {
 
     // standard libraries
-    const val kotlin = "1.5.0"
-    const val coroutines = "1.4.3"
+    const val kotlin = "1.5.20"
+    const val coroutines = "1.5.0"
     const val arrow = "0.13.2"
     const val result = "4.0.0"
 
