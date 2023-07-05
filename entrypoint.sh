2431ae604367c54bc1294e7bb07e6fb30c3f67ae
shariquetaj <sharique.taj@pepro.io>
2023-07-05 13:10:02 +0200

    entrypoint updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 file=$(find data/inputs -type f -name "0" -print -quit)
-cp "$file" data/inputs
+cp "$file" data/inputs && tar -xvf "$file" -C data/inputs --strip-components=1
 
 java -jar /app/app.jar validate-opendrive data/inputs data/outputs
