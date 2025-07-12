242bfa19a9e93724de83dbb108dcde0986eed737
Benedikt Schwab <benedikt.schwab@gmail.com>
2025-07-12 12:14:01 +0200

    updated header years

--- rtron-transformer/src/main/kotlin/io/rtron/transformer/modifiers/opendrive/offset/resolver/OpendriveOffsetResolverReport.kt
+++ rtron-transformer/src/main/kotlin/io/rtron/transformer/modifiers/opendrive/offset/resolver/OpendriveOffsetResolverReport.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2024 Chair of Geoinformatics, Technical University of Munich
+ * Copyright 2019-2026 Chair of Geoinformatics, Technical University of Munich
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
